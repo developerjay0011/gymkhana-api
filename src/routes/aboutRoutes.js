7031971c352e8a6b97346d66989c2cb9e72259c3
Jay Dhimar <jaydhimar1@gmail.com>
2026-04-09 22:56:56 +0530

    Update aboutRoutes.js

--- src/routes/aboutRoutes.js
+++ src/routes/aboutRoutes.js
@@ -8,14 +8,34 @@ const {
   deleteAbout
 } = require('../controllers/aboutController');
 const { protect, admin } = require('../middleware/authMiddleware');
+const { aboutUpload } = require('../middleware/uploadMiddleware');
+
+const uploadFields = aboutUpload.fields([
+  { name: 'coordinator_image', maxCount: 1 },
+  { name: 'director_image', maxCount: 1 },
+  { name: 'assistant_image', maxCount: 1 },
+  { name: 'deputy_director_image', maxCount: 1 }
+]);
+
+// Custom middleware to handle Multer errors
+const handleUpload = (req, res, next) => {
+  uploadFields(req, res, (err) => {
+    if (err) {
+      console.error('Multer Error:', err);
+      return res.status(400).json({ message: 'Upload error', error: err.message });
+    }
+    next();
+  });
+};
 
 // Public routes
 router.get('/', getAboutContent);
 router.get('/:id', getAboutById);
 
 // Protected routes
-router.post('/', [protect, admin], createAbout);
-router.put('/:id', [protect, admin], updateAbout);
+// Moving handleUpload to the front to ensure it parses the stream before other middlewares
+router.post('/', [handleUpload, protect, admin], createAbout);
+router.put('/:id', [handleUpload, protect, admin], updateAbout);
 router.delete('/:id', [protect, admin], deleteAbout);
 
 module.exports = router;
